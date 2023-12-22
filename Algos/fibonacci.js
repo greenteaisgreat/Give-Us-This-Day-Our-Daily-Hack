90f7c134939e3bf140751f9e4f9af34b52a169f2
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2023-12-22 12:36:49 -0600

    Add BST to DS & Fibonacci to Algos Directories

---
+++ Algos/fibonacci.js
@@ -0,0 +1,6 @@
+/*
+Create an algorithm that returns the Fibonacci sequence when its 
+numeric position is entered
+
+Extra credit: try to solve it in O(n) time complexity!
+*/
---
+++ Data-Structures/BST.js
@@ -0,0 +1,7 @@
+/*
+Create a binary search tree that includes four methods:
+  1.) add
+  2.) includes
+  3.) min
+  4.) max
+*/
