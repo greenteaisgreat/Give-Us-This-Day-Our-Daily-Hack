42476b03d39c4180db9c627955d47d300a55b3d9
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2023-12-22 12:37:51 -0600

    Rephrase Fibonacci Prompt

--- Algos/fibonacci.js
+++ Algos/fibonacci.js
@@ -1,6 +1,6 @@
 /*
 Create an algorithm that returns the Fibonacci sequence when its 
-numeric position is entered
+numeric position is entered as an argument
 
 Extra credit: try to solve it in O(n) time complexity!
 */
