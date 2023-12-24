95e1b96c574fa0a45fcc0c722dc499838675adcf
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2023-12-24 15:35:02 -0600

    Add Fibonacci Solution

--- Algos/fibonacci.js
+++ Algos/fibonacci.js
@@ -4,3 +4,19 @@ numeric position is entered as an argument
 
 Extra credit: try to solve it in O(n) time complexity!
 */
+
+// O(2^n) time complexity
+const fibonacci = (n) => {
+  if (n <= 1) return n === 1 ? 1 : 0;
+
+  return fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+// O(n) time complexity
+const tailFib = (n, a = 0, b = 1) => {
+  if (n <= 1) return n === 0 ? a : b;
+
+  return tailFib(n - 1, b, a + b);
+}
+
+console.log(tailFib(7));
