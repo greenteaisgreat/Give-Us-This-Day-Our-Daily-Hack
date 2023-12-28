e17b1a115b0dabe1e7d4b1ddf6df159e91acc107
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2023-12-27 22:14:25 -0600

    Add twoSum Solution

--- Algos/twoSum.js
+++ Algos/twoSum.js
@@ -6,3 +6,26 @@ Extra credit: try to solve it in O(n) time complexity!
 */
 
 //O(n^2) time complexity
+function twoSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arr.length - 1; j++) {
+      if (arr[i] + arr[j] === target) return true;
+    }
+  }
+  return false;
+}
+
+//O(n) time complexity
+const twoSumFast = (arr, target) => {
+  const cache = {};
+
+  for (const index in arr) {
+    const targetSum = target - arr[index];
+
+    if (cache[targetSum]) return true;
+
+    cache[arr[index]] = index;
+  }
+
+  return false;
+}
