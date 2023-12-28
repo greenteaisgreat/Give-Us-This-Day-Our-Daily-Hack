24fa44da017c96574627529dbb9c94bb98a03a35
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2023-12-28 12:33:54 -0600

    Rephrase BST-DFS Prompt

--- Data-Structures/BST-DFS.js
+++ Data-Structures/BST-DFS.js
@@ -1,5 +1,5 @@
 /*
-Perform a depth-first search on a binary search tree; include the three main versions of a DFS:
+Perform a depth-first search (DFS) on a binary search tree; include the three main variations of a DFS:
 1.) Pre-order
 2.) In-order
 3.) Post-order
