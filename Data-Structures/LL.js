34e33bab6e9816a1fd98bdf05aac854c8068d136
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-01-02 11:20:40 -0600

    Rephrase Linked List Prompt

--- Data-Structures/LL.js
+++ Data-Structures/LL.js
@@ -1,5 +1,11 @@
 /*
-Create a singly Linked List that includes two methods:
-  1.) add
-  2.) delete
+Create a singly Linked List that includes eight methods:
+  1.) push
+  2.) pop
+  3.) shift
+  4.) unshift
+  5.) get
+  6.) set
+  7.) insert
+  8.) remove
 */
