338960154317b5536afa85522b7b3c6d589d2bc1
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-01-03 14:08:27 -0600

    Add Solution for intersection.js

--- Algos/intersection.js
+++ Algos/intersection.js
@@ -2,9 +2,28 @@
 Construct a function labeled "intersection" that compares input arrays 
 and returns a new array with elements found in all of the inputs 
 
-ex.)  const arr1 = [5, 10, 15, 20];
-      const arr2 = [15, 88, 1, 5, 7];
-      const arr3 = [1, 10, 15, 5, 20];
+ex.)  
+const arr1 = [5, 10, 15, 20];
+const arr2 = [15, 88, 1, 5, 7];
+const arr3 = [1, 10, 15, 5, 20];
 
-      output should be [5, 15]
+output should be [5, 15]
 */
+
+const arr1 = [5, 10, 15, 20];
+const arr2 = [15, 88, 1, 5, 7];
+const arr3 = [1, 10, 15, 5, 20];
+
+const intersection = (...arr) => {
+	return arr.reduce((arr1, arr2) => {
+		const newArr = [];
+		arr1.forEach(num => {
+			if (arr2.includes(num)) {
+				newArr.push(num);
+			}
+		});
+		return newArr;
+	});
+}
+
+console.log(intersection(arr1, arr2, arr3));
