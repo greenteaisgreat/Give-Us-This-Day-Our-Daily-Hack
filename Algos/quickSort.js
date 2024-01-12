2b6c901fe3f76dd3824bbb06e9bb7e2d874207a3
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-01-12 12:28:20 -0600

    Rephrase quickSort Prompt

--- Algos/quickSort.js
+++ Algos/quickSort.js
@@ -3,5 +3,6 @@ Implement a quick sort algorithm
 
 Quick sort is a comparison sort, meaning that it can sort items 
 of any type for which a "less-than" relation (formally, a total order) is defined.
+
 hint: this is a "divide-and-conquer" based algorithm
 */
