401753b9409530ef0f484422747ffeb3d02cd793
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-01-13 11:07:37 -0600

    Add mergeStrings Solution

--- Algos/mergeStrings.js
+++ Algos/mergeStrings.js
@@ -15,3 +15,30 @@ word2:    p   q   r   s
 merged: a p b q   r   s
 
 */
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @returns {string}
+ */
+
+const mergeStrings = function(word1, word2) {
+  let newStr = '';
+  const longest = word1.length > word2.length ? word1 : word2;
+  
+  for (let i = 0; i < longest.length; i++) {
+  	if (!word1[i] || !word2[i]) {
+    	word1[i] ? newStr += word1[i] : newStr += word2[i];
+    }
+    else {
+    	newStr += word1[i] += word2[i];
+    }
+  }
+  
+  return newStr;
+};
+
+const word1 = 'howdy';
+const word2 = 'pardner';
+
+console.log(mergeStrings(word1, word2));
