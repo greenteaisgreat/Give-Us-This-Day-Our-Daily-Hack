9d011a8494edb00a9b31af6316db261aca7b3cfb
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-08 09:54:26 -0600

    Rephrase longestOnes Prompt

--- Algos/longestOnes.js
+++ Algos/longestOnes.js
@@ -8,7 +8,7 @@ Example 1:
 Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
 Output: 6
 
-Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+Explanation: [1,1,1,0,0,*1*,1,1,1,1,*1*]
 Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
 
 
@@ -18,7 +18,7 @@ Example 2:
 Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
 Output: 10
 
-Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+Explanation: [0,0,1,1,*1*,*1*,1,1,1,*1*,1,1,0,0,0,1,1,1,1]
 Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
 
 
