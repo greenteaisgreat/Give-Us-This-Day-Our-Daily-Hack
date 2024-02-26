29be037f555fbc1b72d88ff8ab2219770913fb18
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-02-26 10:30:52 -0600

    Add guessNumber Solution

--- Algos/guessNumber.js
+++ Algos/guessNumber.js
@@ -53,4 +53,17 @@ const guess = (num, pick = 12345) => {
  * @param {number} n
  * @return {number}
  */
-const guessNumber = (n) => {};
+const guessNumber = (n) => {
+  let l = 0,
+    r = n - 1;
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
+    let res = guess(mid);
+    if (res == 0) return mid;
+    else if (res == 1) l = mid + 1;
+    else r = mid - 1;
+  }
+  return l;
+};
+
+console.log(guessNumber(4));
