a67bcc7e796b5bacedcc85bbe85f2ff54340a0d3
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-04-19 15:35:39 -0500

    Add drawStar Challenge

---
+++ Algos/drawStar.ts
@@ -0,0 +1,23 @@
+/*
+Write a function that logs to the console an nxn overlapping '+' and 'X' for a given 
+number n where n must be an odd positive number. Note that each row of the star must
+be 'n' characters long. Be sure to include any leading/trailing spaces per row.
+
+Examples:
+drawStar(1) ->
++
+
+drawStar(3) ->
+\|/
+-+-
+/|\
+
+drawStar(5) ->
+\ | /
+ \|/ 
+--+--
+ /|\ 
+/ | \
+*/
+
+const drawStar = (n: number): string => {};
