cc696f408c286012a4b56374a64f3d1a15c24e07
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-05-05 18:28:57 -0500

    Reformat Comments for Readability

--- Algos/twoSum.js
+++ Algos/twoSum.js
@@ -3,5 +3,5 @@ Create a twoSum algorithm that returns true or false if any two
 elements' values sum to the passed in target value
 
 Extra credit: try to solve it in O(n) time complexity!
-*/
 
+*/
