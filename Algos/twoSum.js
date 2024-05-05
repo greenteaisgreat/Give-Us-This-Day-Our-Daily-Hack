48fa811441c4a3fa96a89c617a6e4e4ada172d41
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-05-05 18:29:39 -0500

    Reformat Comments for Readability

--- Algos/twoSum.js
+++ Algos/twoSum.js
@@ -3,6 +3,7 @@ Create a twoSum algorithm that returns true or false if any two
 elements' values sum to the passed in target value
 
 Extra credit: try to solve it in O(n) time complexity!
+
 */
 
 const myArr = [1, 2, 3, 4, 5];
