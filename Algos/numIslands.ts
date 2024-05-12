b32f8fe68cb477f6e72bee2b83cc34be420d7fa5
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-05-12 11:47:56 -0500

    Add numIslands Solution

--- Algos/numIslands.ts
+++ Algos/numIslands.ts
@@ -37,4 +37,47 @@ Constraints:
     grid[i][j] is '0' or '1'.
 */
 
-const numIslands = (grid: string[][]): number => {};
+const numIslands = (grid: string[][] | number[][]): number => {
+  //convert from string to number for grid
+  const newGrid = [...grid].map((subArr, i) =>
+    subArr.map((num, i) => (subArr[i] = parseInt(num)))
+  );
+
+  const rows = newGrid.length;
+  const cols = newGrid[0].length;
+  let islands = 0;
+
+  const traverse = (row: number, col: number): void => {
+    //check boundaries
+    if (row < 0 || col < 0) return;
+    if (row >= rows || col >= cols) return;
+    if (!newGrid[row][col]) return;
+
+    //mark as visited
+    newGrid[row][col] = 0;
+
+    traverse(row + 1, col);
+    traverse(row - 1, col);
+    traverse(row, col + 1);
+    traverse(row, col - 1);
+  };
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      if (newGrid[row][col]) {
+        islands++;
+        traverse(row, col);
+      }
+    }
+  }
+  return islands;
+};
+
+const grid = [
+  ["1", "1", "0", "0", "0"],
+  ["1", "1", "0", "0", "0"],
+  ["0", "0", "1", "0", "0"],
+  ["0", "0", "0", "1", "1"],
+];
+
+console.log(numIslands(grid));
