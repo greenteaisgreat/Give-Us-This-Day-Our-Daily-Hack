6e3fe87fadc893df552e863b33c93506d21974f7
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-07-01 17:49:03 -0500

    Add threeConsecutiveOdds Solution

--- Algos/threeConsecutiveOdds.ts
+++ Algos/threeConsecutiveOdds.ts
@@ -23,4 +23,18 @@ Constraints:
     1 <= arr[i] <= 1000
 */
 
-const threeConsecutiveOdds = (arr: number[]): boolean => {};
+const threeConsecutiveOdds = (arr: number[]): boolean => {
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) count++;
+    else count = 0;
+
+    if (count >= 3) return true;
+  }
+
+  return false;
+};
+
+const arr = [1, 2, 34, 3, 4, 5, 7, 23, 12];
+console.log(threeConsecutiveOdds(arr));
