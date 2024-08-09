6fb72989374e42e795334674e027ebe699d7794f
NBornstein278 <nategbornstein@gmail.com>
2024-08-09 16:01:20 -0500

    Add rotate Solution

--- codesignal/rotate.js
+++ codesignal/rotate.js
@@ -25,4 +25,24 @@ Constraints:
     -1000 <= matrix[i][j] <= 1000
 */
 
-const rotate = matrix => {};
+const rotate = matrix => {
+	const n = matrix.length;
+
+	for (let i = 0; i < n; i++) {
+		for (let j = 0; j < i; j++) {
+			[matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+		}
+	}
+
+	matrix.forEach((_, i) => matrix[i].reverse());
+
+	return matrix;
+};
+
+const matrix = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9]
+];
+
+console.log(rotate(matrix));
