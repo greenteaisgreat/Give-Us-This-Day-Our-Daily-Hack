0720a94c523778c0d5ab8ea1a16903902725d5a1
NBornstein278 <nategbornstein@gmail.com>
2024-08-09 15:31:47 -0500

    Add generateMatrix Solution

--- codesignal/generateMatrix.js
+++ codesignal/generateMatrix.js
@@ -18,4 +18,30 @@ Constraints:
     1 <= n <= 20
 */
 
-const generateMatrix = n => {};
+const generateMatrix = n => {
+	const result = [...Array(n)].map(x => []);
+	const half = Math.ceil(n / 2);
+	let counter = 0;
+	let first = 0;
+	let last = n - 1;
+
+	for (let i = 0; i < half; i++) {
+		for (let j = first; j <= last; j++) {
+			result[first][j] = ++counter;
+		}
+		for (let j = first + 1; j <= last; j++) {
+			result[j][last] = ++counter;
+		}
+		for (let j = last - 1; j >= first; j--) {
+			result[last][j] = ++counter;
+		}
+		for (let j = last - 1; j >= first + 1; j--) {
+			result[j][first] = ++counter;
+		}
+		first++;
+		last--;
+	}
+	return result;
+};
+
+console.log(generateMatrix(3));
