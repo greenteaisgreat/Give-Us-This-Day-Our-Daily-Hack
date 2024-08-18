378091e4f635b9d8b6ae40dff92fb2847ef9d85b
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-08-18 09:31:10 -0500

    Add maxDistanceArrays Solution

--- Algos/maxDistanceArrays.ts
+++ Algos/maxDistanceArrays.ts
@@ -31,4 +31,23 @@ Constraints:
     There will be at most 105 integers in all the arrays.
 */
 
-const maxDistanceArrays = (arrays: number[][]): number => {};
+const maxDistanceArrays = (arrays: number[][]): number => {
+  let globalMin = arrays[0][0];
+  let globalMax = arrays[0][arrays[0].length - 1];
+  let result = 0;
+
+  for (let i = 1; i < arrays.length; i++) {
+    const localMin = arrays[i][0];
+    const localMax = arrays[i][arrays[i].length - 1];
+
+    result = Math.max(
+      result,
+      Math.max(localMax - globalMin, globalMax - localMin)
+    );
+
+    globalMin = Math.min(globalMin, localMin);
+    globalMax = Math.max(globalMax, localMax);
+  }
+
+  return result;
+};
