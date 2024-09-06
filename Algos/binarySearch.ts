d9cb9c6eb3f05e4688437623da714e32676f5c70
NBornstein278 <nategbornstein@gmail.com>
2024-09-06 16:20:09 -0500

    Add binarySearch Challenge

---
+++ Algos/binarySearch.ts
@@ -0,0 +1,8 @@
+/*
+Create an algorithm that implements a basic binary search
+on a given array. A binary search is one that halves the data
+set in an object (array) until it finds the desired value. The 
+run-time for these algorithms are typically logarithmic, O(log n)
+*/
+
+const binarySearch = (arr: [], target: number) => {};
