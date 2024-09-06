3cf31cab00f4bc4c87fb11be5aac1a9c7e6d18f9
NBornstein278 <nategbornstein@gmail.com>
2024-09-06 16:29:04 -0500

    Add binarySearch Solution

--- Algos/binarySearch.ts
+++ Algos/binarySearch.ts
@@ -5,4 +5,20 @@ set in an object (array) until it finds the desired value. The
 run-time for these algorithms are typically logarithmic, O(log n)
 */
 
-const binarySearch = (arr: [], target: number) => {};
+const binarySearch = (arr: number[], target: number): boolean => {
+	let low = 0;
+	let high = arr.length;
+
+	while (low < high) {
+		const mid = Math.floor(low + (high - low) / 2);
+		const temp = arr[mid];
+
+		if (temp === target) return true;
+		else if (target > temp) low = mid + 1;
+		else high = mid;
+	}
+	return false;
+};
+
+const arr = [1, 2, 3, 4, 5];
+console.log(binarySearch(arr, 5));
