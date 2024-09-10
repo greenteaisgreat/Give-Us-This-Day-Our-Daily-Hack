c29734c5a5a1731d3f708dabb6eb110ef7af8310
NBornstein278 <nategbornstein@gmail.com>
2024-09-10 14:20:52 -0500

    Add twoCrystalBall Challenge

---
+++ Algos/twoCrystalBalls.ts
@@ -0,0 +1,6 @@
+/*
+Given two crystal balls that will break if dropped from high enough distance, 
+determine the exact spot in which it will break in the most optimized way.
+*/
+
+const twoCrystalBalls = (breaks: boolean[]): boolean | number => {};
