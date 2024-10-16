00a262826a85b398c9e34210ad4f8137bb806182
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-10-16 14:28:56 -0500

    Add squareNSum Solution

--- Algos/squareNSum.ts
+++ Algos/squareNSum.ts
@@ -5,4 +5,16 @@ passed into it and then sums the results together.
 For example, for [1, 2, 2] it should return 9 because 12+22+22=91^2 + 2^2 + 2^2 = 912+22+22=9.
 */
 
-const squareNSum = (list: number[]): number => {};
+const squareNSum = (list: number[]): number => {
+  let ans = 0;
+
+  for (const num of list) {
+    ans += num ** 2;
+  }
+
+  return ans;
+};
+
+const nums = [1, 2, 3];
+
+console.log(squareNSum(nums));
