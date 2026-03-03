16dd089d9acf0ff9756c27b6b4ec778e65a811c8
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2026-03-03 17:14:28 -0600

    Change palindrome

--- Algos/isPalindrome.js
+++ Algos/isPalindrome.js
@@ -4,25 +4,30 @@ argument is a palindrome
 */
 
 const pal = (string) => {
-  if (typeof string !== 'string') throw new Error('Please enter a valid string');
+  if (typeof string !== "string")
+    throw new Error("Please enter a valid string");
 
-  const newStr = string.replace(/[\W]/gi, '').toLowerCase().split('').reverse().join('');
-  
-  return string.replace(/[\W]/gi, '').toLowerCase() === newStr ? true : false;
-}
+  const newStr = string
+    .replace(/[\W]/gi, "")
+    .toLowerCase()
+    .split("")
+    .reverse()
+    .join("");
 
+  return string.replace(/[\W]/gi, "").toLowerCase() === newStr ? true : false;
+};
 
 const palRecursive = (string) => {
-  if (typeof string !== 'string') throw new Error('Please enter a valid string');
-  
-  const newStr = string.replace(/[\W]/gi, '').toLowerCase();
+  if (typeof string !== "string")
+    throw new Error("Please enter a valid string");
 
+  const newStr = string.replace(/[\W]/gi, "").toLowerCase();
 
   if (newStr.length <= 1) return true;
   if (newStr.at(0) !== newStr.at(-1)) return false;
 
   return palRecursive(newStr.slice(1, -1));
-}
+};
 
-console.log(palRecursive('A man a plan a canal panama'));
-console.log(pal('ra c eC A R'));
+console.log(palRecursive("A man a plan a canal panama"));
+console.log(pal("ra c eC A R"));
